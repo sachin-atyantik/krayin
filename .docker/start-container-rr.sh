80fa0c8bc560f2b6160ca60abae02e03ec0f4c01
Maulik Macwan <maulik@atyantik.com>
2024-12-10 11:35:00 +0530

    Adding migration command to roadrunner and swoole

--- .docker/start-container-rr.sh
+++ .docker/start-container-rr.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -e
 
+echo "Reset cache..."
+php artisan config:cache
+
+echo "Running migrations..."
+php artisan migrate --force
+
 echo "Starting RoadRunner..."
 php artisan octane:start --server=roadrunner --port=8081 --host=0.0.0.0
--- .docker/start-container-swoole.sh
+++ .docker/start-container-swoole.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -e
 
+echo "Reset cache..."
+php artisan config:cache
+
+echo "Running migrations..."
+php artisan migrate --force
+
 echo "Starting Swoole..."
 php artisan octane:start --server=swoole --port=8082 --host=0.0.0.0
